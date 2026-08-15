55234a06c2a43be55111538c524d0c9e1791f8ad
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:41:18 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q basiliskii-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q basiliskii-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -12,8 +12,6 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/BasiliskII
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
