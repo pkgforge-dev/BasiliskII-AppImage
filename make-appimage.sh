6a86742695d14620837ff5115c1fb80231b71693
Link4Electronics <link4electronics@gmail.com>
2026-08-18 03:30:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,15 +3,14 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q basiliskii-git | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/BasiliskII
+quick-sharun ./AppDir/bin/BasiliskII
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
